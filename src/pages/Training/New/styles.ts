4c6103ea6c84093836c544ed3913aaa02793e92a
Demétrio Nascimento <devdemetrio@gmail.com>
2026-04-09 11:37:52 -0400

    Adding styled to Training new

---
+++ src/pages/Training/New/styles.ts
@@ -0,0 +1,21 @@
+import {TextField} from '@mui/material'
+import styled from 'styled-components'
+
+
+export const TraningForm = styled.form`
+width: 1024px;
+hidth: 200px;
+margin: 0 auto 0 auto;
+background-color: aqua;
+`
+
+export const WrapperForm = styled.div`
+margin: 1rem 0 1rem 0;
+display: flex;
+flex-direction: column;
+gap: 1rem;
+
+
+`
+
+export const InputDefault  = styled(TextField)``
