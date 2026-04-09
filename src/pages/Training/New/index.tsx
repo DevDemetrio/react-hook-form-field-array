7d5b942596c80dfddbcc834c48e15d97b1a84d69
Demétrio Nascimento <devdemetrio@gmail.com>
2026-04-09 11:37:32 -0400

    Adding screen index to Training new

--- src/pages/Training/New/index.tsx
+++ src/pages/Training/New/index.tsx
@@ -1,5 +1,19 @@
-export function TrainingNew(){
-    return(
-        
-    )
+import { TraningForm, InputDefault, WrapperForm } from "./styles";
+
+export function TrainingNew() {
+  return (
+    <TraningForm>
+      <WrapperForm>
+        <InputDefault
+             label="teste"
+             />
+        <InputDefault
+            label="teste"
+            />
+        <InputDefault 
+            label="teste"
+            />
+      </WrapperForm>
+    </TraningForm>
+  );
 }
